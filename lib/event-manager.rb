dcad6a109aadd5934293e4ef6fa72b0c1d6c38b1
kdoni <chrisvdoni@gmail.com>
2022-11-11 12:06:13 +0200

    Some data analysis.

--- lib/event-manager.rb
+++ lib/event-manager.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 puts 'Event Manager Initialized!'
 
 # content = File.read('event_attendees.csv')
@@ -6,11 +8,28 @@ puts 'Event Manager Initialized!'
 
 # puts File.exist? 'event_attendees.csv'
 
-lines = File.readlines('event_attendees.csv')
-lines.each do |line|
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+# lines = File.readlines('event_attendees.csv')
+# lines.each do |line|
+#   next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
 
-  columns = line.split(',')
-  name = columns[2]
-  puts name
+#   columns = line.split(',')
+#   name = columns[2]
+#   puts name
+# end
+
+# contents = CSV.open('event_attendees.csv', headers: true)
+# contents.each do |row|
+#   name = row[2]
+#   puts name
+# end
+
+contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+
+contents.each do |row|
+  # p row
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  zipcode = '00000' if zipcode.nil?
+  zipcode = '0' + zipcode while zipcode.length < 5
+  puts "#{name} : #{zipcode}"
 end
